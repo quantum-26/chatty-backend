bbdefda541ad1afb12a626cd9224230857bd8a80
quantum-26 <0826pathak@gmail.com>
2022-08-10 14:56:25 +0530

    passing hard cord mongo url - security risk

--- Utilities/Connection.js
+++ Utilities/Connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL || "mongodb+srv://chatty-backend:chatty-backend@chatty.qwyu8v6.mongodb.net/?retryWrites=true&w=majority";
 console.log('mongo url');
 console.log(url);
 const connection = await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
